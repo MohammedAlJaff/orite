99957a0bedcf57996c9f878cb6fc8cb7e4720035
Oskar <oskar.stenerlow@gmail.com>
2019-11-25 14:40:44 +0100

    First version of kmer count
    Functions are imlemented in oritelib and the notebook and some new test files have been added to their respective folders

--- oritelib.py
+++ oritelib.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal, misc, ndimage
+from copy import deepcopy
 
 
 '''
@@ -157,3 +158,84 @@ pos: Numpy array.
 def line_array(k,m,pos):
     y = k*pos + m
     return y
+
+'''
+sequence: string of genome sequence
+kmer_length: int on how long kmers to look for
+circular: Boolean, True by default. Use False if genome is not circular or if a subsequence is used
+
+Output: A list of tuples. First element in tuple represents the kmer (String), the other elelment is a list. The first element in the list 
+represents how many occurences there are of the kmer in the investigated sequence. The following indices store the 
+positions on which the first base in the kmer is located on the sequence. The tuple are based in descending order 
+by how many occurences there are of the kmer in the sequence. 
+''' 
+def get_kmers(sequence, kmer_length, circular = True):
+    kmer = ""
+    kmer_dict = {}
+    sequence_length = len(sequence)
+    
+    if circular:
+        sequence = sequence + sequence[0:kmer_length-1]
+        
+        for i in range(sequence_length):
+            kmer = sequence[i:(i+kmer_length)]
+
+            if kmer in kmer_dict:
+                value_list = kmer_dict[kmer]
+                value_list[0] = value_list[0]+1
+                value_list.append(i)
+                kmer_dict[kmer] = value_list
+
+            else:
+                kmer_dict.update({kmer: [1,i]})
+    else: 
+        for i in range(sequence_length-(kmer_length-1)):
+            kmer = sequence[i:(i+kmer_length)]
+
+            if kmer in kmer_dict:
+                value_list = kmer_dict[kmer]
+                value_list[0] = value_list[0]+1
+                value_list.append(i)
+                kmer_dict[kmer] = value_list
+
+            else:
+                kmer_dict.update({kmer: [1,i]})
+    
+    kmer_dict = sorted(kmer_dict.items(), key = 
+             lambda kv:(kv[1], kv[0]), reverse = True)
+    
+    return kmer_dict
+	
+'''
+kmer_list: A list of tuples. First element in tuple represents the kmer (String), the other elelment is a list. The first element in the list 
+represents how many occurences there are of the kmer in the investigated sequence. The following indices store the 
+positions on which the first base in the kmer is located on the sequence. The tuple are based in descending order 
+by how many occurences there are of the kmer in the sequence. 
+
+n: Number of tuples to return
+
+Output: A list of the n tuples with kmers that occured the most in a sequence
+'''
+def get_top_n_kmers(kmer_list, n):
+    return deepcopy(kmer_list[0:n])
+	
+
+'''
+kmer_list: A tuple. First element represents the kmer (String), the other elelment is a list. The first element in the list 
+represents how many occurences there are of the kmer in the investigated sequence. The following indices store the 
+positions on which the first base in the kmer is located on the sequence. The tuple are based in descending order 
+by how many occurences there are of the kmer in the sequence. 
+
+n: A threshold for how many times a kmer must occur in a sequence for it to be considered relevant
+
+Output: A list of the tuples with kmers that occured more or equal to n number of times in the sequence
+'''
+
+def get_kmer_by_occurence(kmer_list, n):
+    new_kmer_list = []
+    for i in range(len(kmer_list)):
+        if (kmer_list[i][1][0] >= n):
+            new_tuple = deepcopy(kmer_list[i])
+            new_kmer_list.append(new_tuple)
+    return new_kmer_list
+        
